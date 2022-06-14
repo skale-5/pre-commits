cd8b899f15e78774ff89b4612c7ab766be023f75
Thibault Ayanides <thibault.ayanides@skale-5.com>
2022-06-14 11:23:57 +0200

    fix: fix typo

--- scripts/cookiecutter.sh
+++ scripts/cookiecutter.sh
@@ -1,10 +1,10 @@
-#!/busr/bin/env bash
+#!/usr/bin/env bash
 
 
 DIR=$(basename "$(pwd)")
 
 pushd ..
-for test in $(ls tests/*.json); do
+for test in $(ls ${DIR}/tests/*.json); do
     filename=$(basename -- "$test")
     filename="${filename%.*}"
     cookiecutter "$DIR" -f --config-file "$test" --no-input --output-dir "$DIR"/example/"$filename"
