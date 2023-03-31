d69ae6e9473971f47670e8ce36a59d53bfd9535d
Thibault Ayanides <thibault.ayanides@skale-5.com>
2023-03-31 14:12:51 +0200

    chore(gitleaks): disable if command not found
    closes #4

--- scripts/run-gitleaks.sh
+++ scripts/run-gitleaks.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
-DIR=$(basename "$(pwd)")
+if command -v gitleaks; then
+    DIR=$(basename "$(pwd)")
 
-# Copy current repo in /tmp to not disturb dev process
-TMP_DIR=$(mktemp -d /tmp/"$DIR".XXXXXX)
-cp -R "$(pwd)" "$TMP_DIR"
-cd "$TMP_DIR"/"$DIR" || exit 1
+    # Copy current repo in /tmp to not disturb dev process
+    TMP_DIR=$(mktemp -d /tmp/"$DIR".XXXXXX)
+    cp -R "$(pwd)" "$TMP_DIR"
+    cd "$TMP_DIR"/"$DIR" || exit 1
 
-# If git-crypt is used, stash modification, lock repo and apply stash
-# We need to stash because lock cannot be done without
-[ -d ".git/git-crypt" ] &&
-git stash && git-crypt lock && git stash apply
+    # If git-crypt is used, stash modification, lock repo and apply stash
+    # We need to stash because lock cannot be done without
+    [ -d ".git/git-crypt" ] &&
+    git stash && git-crypt lock && git stash apply
 
-# run gitleaks on locked repo
-# it doesn't trigger on files encrypted by git-crypt
-gitleaks protect --verbose --redact --staged
+    # run gitleaks on locked repo
+    # it doesn't trigger on files encrypted by git-crypt
+    gitleaks protect --verbose --redact --staged
 
-# Clean
-#rm -rf "$TMP_DIR"
+    # Clean
+    rm -rf "$TMP_DIR"
+else
+    echo "Gitleaks not found, skipping pre-commit check"
+fi
